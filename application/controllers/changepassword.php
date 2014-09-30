b2dccaa1fe66e76c2788a6fb5dc78f90f9f02e2f
muzani <muzani@velaasia.com>
2014-09-30 16:48:10 +0700

    change password user

---
+++ application/controllers/changepassword.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Changepassword extends MY_Controller {
+	var $data = array();	
+	public function __construct()
+	{
+		$this->allow_group("1");
+		$this->allow_group("cadmin");
+		
+		parent::__construct();
+		$this->data['pagegroup'] = 4;
+		$this->load->model("changepassword_m");
+		$this->data['page'] = "changepassword";
+	}
+	
+	public function index()
+	{
+		$userdata=$this->session->userdata('pkUserId');
+		$data = $this->changepassword_m->getUser($userdata);
+		if($data->num_rows() < 1) {
+			redirect("users");
+		}
+		$this->data['content'] = "form_v.php";
+		$this->data['pageTitle'] = "Change Password";
+		$this->data['breadcrumb'] = array("Change Password"=> "");
+		$this->data['formTitle'] = "Change Password";
+	
+		$this->load->library("va_input", array("group" => "user"));
+		$flashData = $this->session->flashdata("userError");
+		if($flashData !== false) {
+			$flashData = json_decode($flashData, true);
+			$value = $flashData['data'];
+			
+			$msg = $flashData['msg'];
+		} else {
+			$msg = $value = array();
+		}
+	
+		$this->va_input->addHidden( array("name" => "method", "value" => "update") );
+		$this->va_input->addHidden( array("name" => "pkUserId", "value" => $userdata) );		
+		$this->va_input->addPassword( array("name" => "password", "placeholder" => "password", "label" => "Password *", "msg" => @$msg['password'], "value" => @$value['password']) );
+		$this->va_input->addPassword( array("name" => "retypepassword", "placeholder" => "password", "label" => "Re-type Password *", "msg" => @$msg['retypepassword'], "value" => @$value['password']) );		
+		$this->data['script'] = $this->load->view("script/change_password", array(), true);
+		$this->load->view('template', $this->data);
+	}
+	
+	public function save() 
+	{
+	$post = $this->input->post("user");
+		if($post['method'] == "update") 
+		{
+			$result = $this->changepassword_m->changePassword($post);
+			if(is_numeric($result)) 
+			{
+				redirect("users");
+			} 
+			else 
+			{
+			$this->session->set_flashdata( array("userError" => json_encode(array("msg" => $result, "data" => $post))) );
+				redirect("logout");
+			}
+		}
+	}
+	
+	
+	}
---
+++ application/views/script/change_password.php
@@ -0,0 +1,81 @@
+<script>
+var FormValidation = function () {
+
+    var apiFormValidate = function() {
+        // for more info visit the official plugin documentation: 
+            // http://docs.jquery.com/Plugins/Validation
+
+            var form1 = $('#form_<?php echo $this->va_input->getGroup()?>');
+            var error1 = $('.alert-danger', form1);
+            var success1 = $('.alert-success', form1);
+
+            form1.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",
+                messages: {
+                    "user[password]": {
+                        "pwcheck" : "Field should contain character (upper & lower case), number" 
+                    }
+                },
+                rules: {                    
+                    "user[password]": {
+                        minlength: 5,
+                        required: true,
+                        pwcheck: true
+                    },
+					"user[retypepassword]": {
+                    	equalTo: "#password"
+                    }
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit              
+                    success1.hide();
+                    error1.show();
+                    App.scrollTo(error1, -200);
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                    $(element)
+                        .closest('.form-group').addClass('has-error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .closest('.form-group').removeClass('has-error'); // set success class to the control group
+                },
+
+                submitHandler: function (form) {
+                    //success1.show();
+                    error1.hide();
+                    form.submit();
+                }
+            });
+    }
+
+    return {
+        //main function to initiate the module
+        init: function () {
+            apiFormValidate();
+            <?php if($this->router->method == "view"):?>
+            $("#password").rules("remove", "pwcheck");
+            $("input[name='user\[changepass\]']").change(function(){
+            	if($(this).is(":checked")){
+            		$("#password").rules("add", {"pwcheck": true})
+            	} else {
+            		$("#password").rules("remove", "pwcheck");
+            	}
+            })
+            <?php endif;?>
+        }
+
+    };
+
+}();
+</script>
