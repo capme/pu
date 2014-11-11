cef65c78e3c7d2fc94ed24bac0f53c04650a6aa9
muzani <muzani@velaasia.com>
2014-11-11 13:34:30 +0700

    payment confirmation

---
+++ application/controllers/paymentconfirmation.php
@@ -0,0 +1,152 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Paymentconfirmation extends MY_Controller {
+	var $data = array();
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("paymentconfirmation_m");
+		$this->load->model("users_m");
+		$this->load->model("client_m");
+	}
+	
+	public function index() 
+	{
+		$this->data['content'] = "list_v.php";
+		$this->data['pageTitle'] = "Payment Confirmation";
+		$this->data['breadcrumb'] = array("Payment Confirmation" => "");
+				
+		$this->load->library("va_list");
+		$this->va_list->setListName("Payment Confirmation")->setMassAction(array("2" => "Remove"))
+		->setHeadingTitle(array("Record #", "Client Name","Order Number","Name","Origin Bank","Transact Mtd","Status","Transact Date","Receipt","Updated By"))
+		->setHeadingWidth(array(2, 2,2,3,2,3,4,2,2,4));
+		
+		$this->va_list->setInputFilter(2, array("name" => $this->paymentconfirmation_m->filters['order_number']));
+		$this->va_list->setDropdownFilter(6, array("name" => $this->paymentconfirmation_m->filters['status'], "option" => $this->getStatus()));
+		
+		$this->data['script'] = $this->load->view("script/paymentconfirmation_list", array("ajaxSource" => site_url("paymentconfirmation/paymentConfirmationList")), true);	
+		$this->load->view("template", $this->data);
+	}
+	
+	public function paymentConfirmationList()
+	{
+		$sAction = $this->input->post("sAction");
+		if($sAction == "group_action") {
+			$id = $this->input->post("id");
+			if(sizeof($id) > 0) {
+				$action = $this->input->post("sGroupActionName");
+				$this->paymentconfirmation_m->removePayment($id, $action);
+			}
+		}	
+		$data = $this->paymentconfirmation_m->getPaymentConfirmationList();	
+		echo json_encode($data);
+	}
+	
+	public function view($id)
+	{
+		$data = $this->paymentconfirmation_m->getConfirmationById($id);
+		if($data->num_rows() < 1) {
+			redirect("paymentconfirmation");
+		}
+		
+		$this->data['content'] = "form_v.php";
+		$this->data['pageTitle'] = "Payment Confirmation";
+		$this->data['breadcrumb'] = array("Payment Confirmation"=> "", "View Payment Confirmation" => "");
+		$this->data['formTitle'] = "View Payment Confirmation";
+	
+		$this->load->library("va_input", array("group" => "returnorder"));
+		$this->va_input->setJustView();
+		$flashData = $this->session->flashdata("clientError");
+		if($flashData !== false) {
+			$flashData = json_decode($flashData, true);
+			$value = $flashData['data'];
+			$msg = $flashData['msg'];
+		} else {
+			$msg = array();
+			$value = $data->row_array();
+		}
+		
+		$this->va_input->addInput( array("name" => "client_code", "placeholder" => "Client Code", "help" => "Client Code", "label" => "Client Code *", "value" => @$value['client_code'], "msg" => @$msg['client_code']) );
+		
+		$this->va_input->addInput( array("name" => "sku", "placeholder" => "Order Number", "help" => "Order Number", "label" => "Order Number *", "value" => @$value['order_number'], "msg" => @$msg['order_number']) );
+		$this->va_input->addSelect( array("name" => "status", "list" => $this->getStatus(), "value" => @$value['status'], "msg" => @$msg['status'], "label" => "Order Status *", "help" => "Order Status") );		
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['name'], "msg" => @$msg['name'], "label" => "Name *", "help" => "Name") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['transaction_date'], "msg" => @$msg['transaction_date'], "label" => "Transaction Date *", "help" => "Transaction Date") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['amount'], "msg" => @$msg['amount'], "label" => "Amount *", "help" => "Amount") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['origin_bank'], "msg" => @$msg['origin_bank'], "label" => "Original Bank *", "help" => "Original Bank") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['dest_bank'], "msg" => @$msg['dest_bank'], "label" => "Destination Bank *", "help" => "Destination Bank") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['transaction_method'], "msg" => @$msg['transaction_method'], "label" => "Transaction Method *", "help" => "Transaction Method") );
+		
+		$this->va_input->addTextarea( array("name" => "cancel_reason", "value" => @$value['reason'], "msg" => @$msg['reason'], "label" => "Reason*", "help" => "Reason") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['updated_at'], "msg" => @$msg['updated_at'], "label" => "Updated At *", "help" => "Updated At") );
+		$this->va_input->addInput( array("name" => "updated_at", "value" => @$value['created_at'], "msg" => @$msg['created_at'], "label" => "Created At *", "help" => "Created At") );
+		
+		$this->va_input->addCustomField( array("name" =>"receipt_url", "placeholder" => "xx", "label" => "Receipt URL", "value" => @$value['receipt_url'], "msg" => @$msg['receipt_url'], "view"=>"form/customFoto"));		
+		
+		$this->data['script'] = $this->load->view("script/client_add", array(), true);
+		$this->load->view('template', $this->data);
+	}
+	
+	private function getStatus() {
+		return array(0=>"Default",1 => "Approve",2 => "Cancel");
+	}
+	
+	public function approve ($id)
+	{
+		$this->paymentconfirmation_m->Approve($id);
+		redirect("paymentconfirmation");
+	}
+	
+	public function cancel($id)
+	{
+		$data = $this->paymentconfirmation_m->getConfirmationById($id);
+		if($data->num_rows() < 1) {
+			redirect("paymentconfirmation");
+		}
+		
+		$this->data['content'] = "form_v.php";
+		$this->data['pageTitle'] = "Cancel Item";
+		$this->data['breadcrumb'] = array("Payment Confirmation "=> "", "Cancel Payment" => "");
+		$this->data['formTitle'] = "cancel payment";
+	
+		$this->load->library("va_input", array("group" => "returnorder"));
+		$flashData = $this->session->flashdata("clientError");
+		if($flashData !== false) {
+			$flashData = json_decode($flashData, true);
+			$value = $flashData['data'];
+			$msg = $flashData['msg'];
+		} else {
+			$msg = array();
+			$value = $data->row_array();
+		}
+	
+		$this->va_input->addHidden( array("name" => "method", "value" => "update") );
+		$this->va_input->addHidden( array("name" => "id", "value" => $value['id']) );
+		$this->va_input->addTextarea( array("name" => "reason", "placeholder" => "Cancel reason", "help" => "Cancel reason", "label" => "Cancel Reason *", "value" => @$value['reason'], "msg" => @$msg['reason']) );
+		
+		$this->data['script'] = $this->load->view("script/client_add", array(), true);
+		$this->load->view('template', $this->data);
+	}
+	
+	public function save() 
+	{
+		$post = $this->input->post("returnorder");
+		if(empty($post)) {
+			redirect("paymentconfirmation");
+		}
+		
+		else if($post['method'] == "update") {
+			$result = $this->paymentconfirmation_m->Reason($post);
+			if(is_numeric($result)) 
+			{
+				redirect("paymentconfirmation");
+			} 
+			else 
+			{
+				$this->session->set_flashdata( array("clientError" => json_encode(array("msg" => $result, "data" => $post))) );
+				redirect("paymentconfirmation/cancel/".$post['id']);
+			}
+		}
+	}
+	
+	
+}
---
+++ application/models/paymentconfirmation_m.php
@@ -0,0 +1,130 @@
+<?php
+class Paymentconfirmation_m extends MY_Model {
+	var $filterSession = "DB_USER_FILTER";
+	var $db = null;
+	var $table = 'bank_confirmation';
+	var $sorts = array(1 => "id");
+	var $pkField = "id";
+	var $status=array("cancel"=>2,"approve"=>1);
+	var $tableClient ='client';
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->db = $this->load->database('mysql', TRUE);
+		
+		$this->relation = array(
+			array("type" => "inner", "table" => $this->tableClient, "link" => "{$this->table}.client_id  = {$this->tableClient}.{$this->pkField}")
+		);
+		
+		$this->select = array("{$this->table}.{$this->pkField}", "{$this->table}.order_number", "{$this->table}.origin_bank", "{$this->table}.dest_bank", "{$this->table}.transaction_method", "{$this->table}.amount", "{$this->table}.name", "{$this->table}.transaction_date", "{$this->table}.updated_at", "{$this->table}.status", "{$this->table}.receipt_url","{$this->table}.updated_by", "{$this->tableClient}.client_code");
+		
+		$this->filters = array("status"=>"status","order_number"=>"order_number");
+	}
+	
+		public function getPaymentConfirmationList() 
+	{
+		$this->db = $this->load->database('mysql', TRUE); 
+		$iTotalRecords = $this->_doGetTotalRow();
+		$iDisplayLength = intval($this->input->post('iDisplayLength'));
+		$iDisplayLength = $iDisplayLength < 0 ? $iTotalRecords : $iDisplayLength;
+		$iDisplayStart = intval($this->input->post('iDisplayStart'));
+		$sEcho = intval($this->input->post('sEcho'));
+	
+		$records = array();
+		$records["aaData"] = array();
+		
+		$grup=$this->users_m->userList();
+		$opsiarray=array();
+		foreach($grup as $pkUserId=>$row)
+		{
+		$opsiarray[$row['pkUserId']]=$row['username'];
+		}
+		
+		$statList= array(
+				0=>"Default",
+				1 => "Approve",
+				2 => "Cancel"
+		);
+		
+		$end = $iDisplayStart + $iDisplayLength;
+		$end = $end > $iTotalRecords ? $iTotalRecords : $end;
+	
+		$_row = $this->_doGetRows($iDisplayStart, $iDisplayLength);
+		$no=0;
+		foreach($_row->result() as $_result) {
+			
+			$records["aaData"][] = array(
+					'<input type="checkbox" name="id[]" value="'.$_result->id.'">',
+					$no=$no+1,
+					$_result->client_code,
+					$_result->order_number,
+					$_result->name,
+					$_result->origin_bank,
+					$_result->transaction_method,					
+					$statList[$_result->status],
+					$_result->transaction_date,					
+					'<a '.anchor($_result->receipt_url,'View','target="_blank" enabled="enabled" class="fa fa-search btn btn-xs default"').'</a>',		
+					
+					@$opsiarray[$_result->updated_by],					
+					'<a href="'.site_url("paymentconfirmation/view/".$_result->id).'"  enabled="enabled" class="btn btn-xs default"><i class="fa fa-search" ></i> View</a>|			
+					<a href="'.site_url("paymentconfirmation/approve/".$_result->id).'" class="btn btn-xs default"><i class="fa fa-check" ></i> Approve</a>|
+					<a href="'.site_url("paymentconfirmation/cancel/".$_result->id).'" class="btn btn-xs default"><i class="fa fa-times" ></i> Cancel</a>'
+									
+			);
+		}
+	
+		$records["sEcho"] = $sEcho;
+		$records["iTotalRecords"] = $iTotalRecords;
+		$records["iTotalDisplayRecords"] = $iTotalRecords;
+		return $records;
+	}
+	
+	public function getConfirmationById($id)
+	{
+		$this->db = $this->load->database('mysql', TRUE);
+		$this->db->select('*, bank_confirmation.id, bank_confirmation.updated_at');
+		$this->db->from($this->table);
+		$this->db->join('client','client.id=bank_confirmation.client_id');
+		$this->db->where('bank_confirmation.id', $id);		
+		return $this->db->get();  
+	}
+	
+	public function Approve ($id)
+	{
+		$this->db = $this->load->database('mysql', TRUE);
+		$user=$this->session->userdata('pkUserId');	
+		$time=date('Y-m-d H:i:s', now());
+		
+		$data['status'] = $this->status['approve'];
+		$data['updated_by']=$user;
+		$data['updated_at']= $time;	
+		
+		$this->db->where($this->pkField, $id);
+		$this->db->update($this->table, $data);
+		return $id;		
+	}
+	
+	public function Reason($post) 
+	{
+		$msg = array();			
+		$user=$this->session->userdata('pkUserId');		
+		$time=date('Y-m-d H:i:s', now());
+		
+		if(!empty($post['reason'])) {
+			$data['reason'] = $post['reason'];
+			$data['status'] = $this->status['cancel'];
+			$data['updated_by']=$user;
+			$data['updated_at']=$time;	
+			
+			$this->db->where($this->pkField, $post['id']);			
+			$this->db->update($this->table, $data);
+			return $post['id'];
+		} 
+		else {
+			return $msg;
+		}	
+	}
+	
+	}
+?>
