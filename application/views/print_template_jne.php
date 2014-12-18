daf3ce21f176f5068e46207054fe854f082106d3
muzani <zanimumu@gmail.com>
2014-12-18 11:26:11 +0700

    print awb JNE and NEX with firefox format

--- application/views/print_template_jne.php
+++ application/views/print_template_jne.php
@@ -1,42 +1,99 @@
 <style>
+@import url('/assets/css/normalize.css') print;
 @font-face {
   font-family: 'FXMatrix105MonoEliteExpDbl';
-  src: url('<?php echo base_static()?>fonts/FXMatrix105MonoEliteExpDblRegular.woff') format('woff');
+  src: url('<?php echo base_static()?>/FXMatrix105MonoEliteExpDblRegular.woff') format('woff');
 }
-body{
+body.Firefox{
+	margin: 0px;
+	padding: 0px;	
+}
+
+body.Chrome{
 	margin: 0px;
 	padding: 0px;
 }
-.area > div {
+
+.Firefox .area{ 
+	font-family: FXMatrix105MonoEliteExpDbl; 
+	font-size: 6px; 0cm; 
+	border: 1px solid white;
+	width: 19cm; 
+	height: 9.3cm; 
+	position: relative; 
+	letter-spacing:.5; 
+	margin-bottom:0.7cm; 
+	margin-top:14px; 
+	margin-left: -15px;
+}
+
+.Chrome .area{
+	font-family: FXMatrix105MonoEliteExpDbl; 
+	font-size: 6px; 0cm; 
+	border: 1px solid white;
+	width: 19cm; 
+	height: 5cm; 
+	position: relative; 
+	letter-spacing:.5; 
+	margin-bottom:0.7cm; 
+	margin-top:14px; 
+	margin-left: -15px;
+}
+.Firefox .area > div{
+    border-bottom: 1px solid blue;
+	border-right: 1px solid blue;
+    width: 1cm;
+    height: 0.5cm;
+}
+
+.Chrome .area > div {
     border-bottom: 1px solid blue;
 	border-right: 1px solid blue;
     width: 1cm;
     height: 0.5cm;
     display: inline-block;
 }
-.container{position: absolute; border:1px solid gray !important;}
-*, .container {border: none !important;}
-.area{border-bottom: 1px solid white !important;}
-.container.delivery-ins{left: 1.6cm; top: 3.2cm; width: 7cm; height: 0.5cm;}
-.container.print-date{left:4.7cm; top: 4.2cm; height: 0.4cm; width:2cm;}
-.container.package-type{left: 16.3cm; top: 4cm; width: .5cm; height: 0.3cm; font-size:11px;}
-.container.shipping-type{left: 16.3cm; top: 1.8cm; width: .5cm; height: 0.5cm; font-size:11px;}
-.container.receiver{left: 9.4cm; top: 0.9cm; width: 5.5cm; height: 0.2cm;}
-.container.company{left: 10.4cm; top: 1.1cm; width: 6cm; height: 0.2cm;}
-.container.addr2{left: 13.4cm; top: 1.8cm; width: 2cm; height: 0.8cm; font-size:5px;}
-.container.city{left: 8.9cm; top: 1.8cm; width: 3cm; height: 0.12cm;}
-.container.prov{left: 10cm; top: 2cm; width: 2.5cm; height: 0.2cm;}
-.container.addr1{left: 9.4cm; top: 1.3cm; width: 6.4cm; height: 0.6cm;}
-.container.items{left: 9cm; top: 2.7cm; width: 4cm; height: 1cm;}
-.items span{display:inline-block; vertical-align: top;}
-.items .name{font-size: 5px; width: 2.8cm; }
-.items .qty{width: 1cm; }
+
+.Firefox .container{position: absolute; border:1px solid gray !important;}
+*, .Firefox .container {border: none !important;}
+.Firefox .container.delivery-ins{left: 1.6cm; top: 5.3cm; width: 7cm; height: 0.5cm;font-size: 11px;}
+.Firefox .container.print-date{left:6.5cm; top: 7cm; height: 0.4cm; width:3cm; font-size: 9px;}
+.Firefox .container.package-type{left: 27.5cm; top: 6.3cm; width: .5cm; height: 0.3cm; font-size:12px;}
+.Firefox .container.shipping-type{left: 27.5cm; top: 2.9cm; width: .5cm; height: 0.5cm; font-size:12px;}
+.Firefox .container.receiver{left: 16cm; top: 1.5cm; width: 5.5cm; height: 0.2cm;font-size: 12px;}
+.Firefox .container.company{left: 10.4cm; top: 1.1cm; width: 6cm; height: 0.2cm;font-size: 9px;}
+.Firefox .container.addr2{left: 22.4cm; top: 2.7cm; width: 2cm; height: 0.8cm; font-size:9px;}
+.Firefox .container.city{left: 14.4cm; top: 2.7cm; width: 3cm; height: 0.12cm; font-size: 9px;}
+.Firefox .container.prov{left: 16.3cm; top: 3cm; width: 2.5cm; height: 0.2cm; font-size: 9px;}
+.Firefox .container.addr1{left: 14.9cm; top: 1.9cm; width: 6.4cm; height: 0.6cm; font-size: 9px;}
+.Firefox .container.items{left: 14.1cm; top: 4.2cm; width: 6cm; height: 1cm; font-size: 9px;}
+.Firefox .items span{display:inline-block; vertical-align: top; font-size: 9px;}
+.Firefox .items .name{font-size: 9px; width: 4cm; }
+.Firefox .items .qty{width: 1cm; font-size: 9px;}
+
+.Chrome .container{position: absolute; border:1px solid gray !important;}
+*, .Chrome .container {border: none !important;}
+.Chrome .container.delivery-ins{left: 1.6cm; top: 3.2cm; width: 7cm; height: 0.5cm;}
+.Chrome .container.print-date{left:4.2cm; top: 4.2cm; height: 0.4cm; width:2cm;}
+.Chrome .container.package-type{left: 16cm; top: 3.8cm; width: .5cm; height: 0.3cm; font-size:11px;}
+.Chrome .container.shipping-type{left: 16cm; top: 1.9cm; width: .5cm; height: 0.5cm; font-size:11px;}
+.Chrome .container.receiver{left: 9.4cm; top: 0.9cm; width: 5.5cm; height: 0.2cm;}
+.Chrome .container.company{left: 10.4cm; top: 1.1cm; width: 6cm; height: 0.2cm;}
+.Chrome .container.addr2{left: 13.1cm; top: 1.8cm; width: 2cm; height: 0.8cm; font-size:5px;}
+.Chrome .container.city{left: 8.7cm; top: 1.7cm; width: 3cm; height: 0.12cm;}
+.Chrome .container.prov{left: 9.8cm; top: 1.9cm; width: 2.5cm; height: 0.2cm;}
+.Chrome .container.addr1{left: 9.1cm; top: 1.3cm; width: 6.4cm; height: 0.6cm;}
+.Chrome .container.items{left: 8.2cm; top: 2.7cm; width: 4cm; height: 1cm;}
+.Chrome .items span{ display:inline-block; vertical-align: top;}
+.Chrome .items .name{ font-size: 5px; width: 2.4cm; }
+.Chrome .items .qty{width: 1cm;}
+
 @page {
 	margin: 0.3cm;
 }
 </style>
-
 <?php 
+$browser = get_browser(null, true);
 $grup=$this->client_m->getClients();
 $opsi=array();
 foreach($grup as $id=>$row)
@@ -50,7 +107,8 @@ echo "<div style='page-break-before: always;'></div><div style='height:2px;'></d
 }
 $client=$opsi[$v->client_id];
 ?>
-<div style="font-family: FXMatrix105MonoEliteExpDbl; font-size: 6px; 0cm; border: 1px solid white; width: 19cm; height: 5cm;  position: relative; letter-spacing:.5; margin-bottom:0.7cm; margin-top:14px; margin-left: -15px;" class="area">
+<body class="<?php echo $browser['browser'];?>">
+<div  class="area">
 	<div class="container delivery-ins"><?php echo $v->ordernr." - ".$client?></div>
 	<div class="container print-date"><?php echo date("d m Y", time())?></div>
 	<div class="container package-type">X</div>
@@ -76,11 +134,12 @@ $client=$opsi[$v->client_id];
 			if(empty($i)){continue;}
 			else{$i = unserialize($i);}
 		?>
-		<div><span class="name"><?php echo $i['name']?></span><span class="qty"><?php echo $i['qty']?>&nbsp;&nbsp;<?php echo $i['weight']?></span></div>
+		<div><span class="name"><?php echo $i['name']?></span><span class="qty"><?php echo $i['qty']?>&nbsp;&nbsp<?php echo $i['weight']?></span></div>
 		<?php endforeach; ?>
 	</div>
   <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
 </div>
+</body>
 <?php endforeach; ?>
 <script>
 window.print();
--- application/views/print_template_nex.php
+++ application/views/print_template_nex.php
@@ -1,44 +1,104 @@
 <style>
 @font-face {
   font-family: 'FXMatrix105MonoEliteExpDbl';
-  src: url('<?php echo base_static()?>fonts/FXMatrix105MonoEliteExpDblRegular.woff') format('woff');
+  src: url('<?php echo base_static()?>/FXMatrix105MonoEliteExpDblRegular.woff') format('woff');
 }
-body{
+body .Firefox{
 	margin: 0px;
 	padding: 0px;
 }
-.area > div {
+
+body .Chrome{
+	margin: 0px;
+	padding: 0px;
+}
+
+.Firefox .area{
+	border-bottom:1px solid #000000 !important;
+	font-family: FXMatrix105MonoEliteExpDbl; 
+	font-size: 6px; 0cm; 
+	border: 1px solid white; 
+	width: 19cm; 
+	height: 9.3cm;  
+	position: relative; 
+	letter-spacing:.5; 
+	margin-bottom:0.7cm; 
+	margin-top:14px; 
+	margin-left: -15px;
+}
+
+.Chrome .area{
+	font-family: FXMatrix105MonoEliteExpDbl; 
+	font-size: 6px; 0cm; 
+	border: 1px solid white; 
+	width: 19cm; 
+	height: 5cm;  
+	position: relative; 
+	letter-spacing:.5; 
+	margin-bottom:0.7cm; 
+	margin-top:14px; 
+	margin-left: -15px;
+}
+.Firefox .area > div {
     border-bottom: 1px solid blue;
 	border-right: 1px solid blue;
     width: 1cm;
     height: 0.5cm;
     display: inline-block;
 }
-.container{position: absolute; border:1px solid gray !important;}
-*, .container {border: none !important;}
-.area{border-bottom: 1px solid white !important;}
-.container.delivery-ins{left: 6cm; top: 3.2cm; width: 7cm; height: 0.5cm;}
-.container.print-date{left:8.6cm; top: 4.2cm; height: 0.4cm; width:2cm;}
-.container.package-type{left: 1.2cm; top: 3.9cm; width: .5cm; height: 0.3cm; font-size:11px;}
-.container.shipping-type{left: 1.2cm; top: 1.7cm; width: .5cm; height: 0.5cm; font-size:11px;}
-.container.receiver{left: 13.3cm; top: 0.9cm; width: 5.5cm; height: 0.2cm;}
-.container.company{left: 13.3cm; top: 1.1cm; width: 6cm; height: 0.2cm;}
-.container.addr2{left: 17.3cm; top: 1.7cm; width: 2cm; height: 0.8cm; font-size:5px;}
-.container.city{left: 13.3cm; top: 1.7cm; width: 3cm; height: 0.12cm;}
-.container.prov{left: 13.8cm; top: 1.9cm; width: 2.5cm; height: 0.2cm;}
-.container.addr1{left: 13.3cm; top: 1.3cm; width: 6.4cm; height: 0.6cm;}
-.container.items{left: 12.3cm; top: 2.7cm; width: 4cm; height: 1cm;}
-.items span{display:inline-block; vertical-align: top;}
-.items .name{font-size: 5px; width: 2.8cm; }
-.items .qty{width: 0.8cm; }
-.Firefox .container.nilai{left: 25.2cm; top: 7.5cm; width: 2.5cm; height: 0.2cm; font-size: 9px;}
+
+.Chrome .area > div {
+    border-bottom: 1px solid blue;
+	border-right: 1px solid blue;
+    width: 1cm;
+    height: 0.5cm;
+    display: inline-block;
+}
+
+
+.Firefox .container{position: absolute; border:1px solid gray !important;}
+*, .Firefox .container {border: none !important;}
+.Firefox .container.delivery-ins{left: 7cm;  top: 5.2cm; width: 7cm; height: 0.6cm; font-size: 11px;}
+.Firefox .container.print-date{left:13.6cm; top: 6.9cm; height: 0.5cm; width:4cm; font-size: 11px;}
+.Firefox .container.package-type{left: 0.3cm; top: 7.1cm; width: .5cm; height: 0.3cm; font-size:12px;}
+.Firefox .container.shipping-type{left: 0.3cm; top: 3.4cm; width: .5cm; height: 0.5cm; font-size:12px;}
+.Firefox .container.receiver{left: 22cm;  top: 1.3cm; width: 5.5cm; height: 0.2cm; font-size: 11px;}
+.Firefox .container.company{left: 13.3cm; top: 1.1cm; width: 6cm; height: 0.2cm; font-size: 9px;}
+.Firefox .container.addr2{left: 29.4cm; top: 2.5cm; width: 2cm; height: 0.8cm; font-size:9px;}
+.Firefox .container.city{left: 21.9cm;  top: 2.5cm; width: 3cm; height: 0.12cm; font-size: 9px;}
+.Firefox .container.prov{left: 22.8cm; top: 2.8cm; width: 2.5cm; height: 0.2cm; font-size: 9px;}
+.Firefox .container.addr1{left: 21.9cm; top: 1.7cm; width: 6.4cm; height: 0.6cm; font-size: 11px;}
+.Firefox .container.nilai{left: 25.2cm; top: 7.7cm; width: 2.5cm; height: 0.2cm; font-size: 11px;}
+.Firefox .container.items{left: 21.1cm; top: 4.2cm; width: 7cm; height: 1cm; font-size: 11px;}
+.Firefox .items span{display:inline-block; vertical-align: top;}
+.Firefox .items .name{font-size: 9px; width: 3.6cm; }
+.Firefox .items .qty{font-size: 9px;width: 1.2cm; }
+
+.Chrome .container{position: absolute; border:1px solid gray !important;}
+*, .Chrome .container {border: none !important;}
+.Chrome .container.delivery-ins{left: 5.5cm; top: 3.2cm; width: 7cm; height: 0.5cm;}
+.Chrome .container.print-date{left:8.1cm; top: 4.1cm; height: 0.4cm; width:2cm;}
+.Chrome .container.package-type{left: 0.7cm; top: 3.8cm; width: .5cm; height: 0.3cm; font-size:11px;}
+.Chrome .container.shipping-type{left: 0.7cm; top: 1.6cm; width: .5cm; height: 0.5cm; font-size:11px;}
+.Chrome .container.receiver{left: 12.9cm; top: 0.9cm; width: 5.5cm; height: 0.2cm;}
+.Chrome .container.company{left: 13.3cm; top: 1.1cm; width: 6cm; height: 0.2cm;}
+.Chrome .container.addr2{left: 16.9cm; top: 1.7cm; width: 2cm; height: 0.8cm; font-size:5px;}
+.Chrome .container.city{left: 12.9cm; top: 1.7cm; width: 3cm; height: 0.12cm;}
+.Chrome .container.prov{left: 13.2cm; top: 1.9cm; width: 2.5cm; height: 0.2cm;}
+.Chrome .container.addr1{left: 13cm; top: 1.3cm; width: 6.4cm; height: 0.6cm;}
+.Chrome .container.items{left: 12.3cm; top: 2.7cm; width: 4cm; height: 1cm;}
 .Chrome .container.nilai{left: 15.8cm; top: 4.5cm; width: 2.5cm; height: 0.2cm;}
+.Chrome .items span{display:inline-block; vertical-align: top;}
+.Chrome .items .name{font-size: 5px; width: 2.2cm; }
+.Chrome .items .qty{width: 1.2cm; } 
+
 @page {
 	margin: 0.3cm;
 }
 </style>
 
 <?php
+$browser = get_browser(null, true);
 $grup=$this->client_m->getClients();
 $opsi=array();
 foreach($grup as $id=>$row)
@@ -52,11 +112,12 @@ echo "<div style='page-break-before: always;'></div><div style='height:2px;'></d
 }
 $client=$opsi[$v->client_id];
 ?>
-<div style="font-family: FXMatrix105MonoEliteExpDbl; font-size: 6px; 0cm; border: 1px solid white; width: 19cm; height: 5cm;  position: relative; letter-spacing:.5; margin-bottom:0.7cm; margin-top:14px; margin-left: -15px;" class="area">
+<body class="<?php echo $browser['browser'];?>">
+<div class="area">
 	<div class="container delivery-ins"><?php echo $v->ordernr." - ".$client?></div>
 	<div class="container print-date"><?php echo date("d m Y", time())?></div>
 	<div class="container package-type">X</div>
-	<div class="container shipping-type"><?php echo ($v->shipping_type == "YES") ? "X" : "<br />X"?></div>
+	<div class="container shipping-type"><?php echo ($v->shipping_type == "1") ? "A" : ($v->shipping_type == "2" ? "<br /> <br /> <br /> B" : "<br />C")?></div>
 	<div class="container receiver"><?php echo $v->receiver?></div>
 	<div class="container company"><?php echo $v->company?></div>
 	<div class="container addr2"><?php echo $v->zipcode."<br />".$v->country."<br />".$v->phone?></div>
@@ -84,6 +145,7 @@ $client=$opsi[$v->client_id];
 	</div>
   <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
 </div>
+</body>
 <?php endforeach; ?>
 <script>
 window.print();
