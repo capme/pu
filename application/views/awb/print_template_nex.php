de2e72f4bd29d567c93ad5435383a56c82abeeb4
muzani <zanimumu@gmail.com>
2015-01-20 09:48:26 +0700

    AWB NEX export PDF

--- application/views/awb/print_template_nex.php
+++ application/views/awb/print_template_nex.php
@@ -42,39 +42,62 @@ $pdf->SetFont('calibri', '', 9);
 /** start loop **/
 $pdf->AddPage('L');
 // create some HTML content
+$grup=$this->client_m->getClients();
+$opsi=array();
+foreach($grup as $id=>$row)
+	{
+	$opsi[$row['id']] = $row['client_code'];
+	}
+$date= date("d m Y", time());
+foreach ($list->result() as $hasil => $data):
+
+$client=$opsi[$data->client_id];
+
+$addr = explode("\n", $data->address);
+	if(sizeof($addr) > 3) {
+		$data->address = implode(" ", $addr);
+	} else {
+		$data->address = implode("<br />", $addr);
+	}
+	
+$itemLists = explode("|", $data->itemlist);
+$items = array(array('', '', ''), array('', '', ''), array('', '', ''), array('', '', ''));
+foreach($itemLists as $l => $item) {
+	$i = unserialize($item);	
+	$items[$l] = array($i['name'], $i['qty'], $i['weight']);
+}
+
+$number=number_format($data->amount, 0);
 $html = <<<EOF
 <table border="0" cellspacing="0" cellpadding="0">
 	<tr>
 		<td width="25%"></td>
 		<td width="25%"></td>
 		<td width="20%"></td>
-		<td width="35%">Sylviana Hamdani</td>
+		<td width="35%">$data->receiver</td>
 	</tr>
 	<tr>
 		<th width="42%"></th>
 		<th width="25%"></th>
-		<th height="41" width="33%" colspan="2" style="font-size:8pt;">Jl. Setiabudi Tengah no. 7, sekitar dua
-rumah dari restoran Mbah Jingkrak,
-Karet Selatan
-- Setiabudi, Jakarta Selatan</th>
+		<th height="41" width="33%" colspan="2" style="font-size:8pt;">$data->address</th>
 	</tr>
 	<tr>
 		<th width="42%"></th>
 		<td width="25%"></td>
-		<td width="22%">Kab. Tangerang</td>
-		<td width="11%">15143</td>
+		<td width="22%">$data->city</td>
+		<td width="11%">$data->zipcode</td>
 	</tr>
 	<tr>
 		<th width="45%"></th>
 		<th width="26%"></th>
-		<th width="18%">Banten</th>
-		<th width="11%">Indonesia</th>
+		<th width="18%">$data->province</th>
+		<th width="11%">$data->country</th>
 	</tr>
 	<tr>
 		<th width="45%"></th>
 		<th width="26%"></th>
 		<th width="18%"></th>
-		<th width="13%">0812377483</th>
+		<th width="13%">$data->phone</th>
 	</tr>
 		<tr>
 		<th width="45%"></th>
@@ -89,34 +112,29 @@ Karet Selatan
 		<th width="38%"></th>
 	</tr>
 		<tr>
-		<th width="62%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; X</th>
-		<th width="15%">&nbsp;&nbsp;&nbsp; SKU</th>
-		<th width="5%">1</th>
-		<th width="18%">1</th>
+		<th width="62%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </th>
+		<th width="15%">&nbsp;&nbsp;&nbsp; {$items[0][0]}</th>
+		<th width="5%">{$items[0][1]}</th>
+		<th width="18%">{$items[0][2]}</th>
 	</tr>
 	<tr>
 		<th width="62%"></th>
-		<th width="15%">&nbsp;&nbsp;&nbsp; SKU</th>
-		<th width="5%">1 </th>
-		<th width="18%">1</th>
+		<th width="15%">&nbsp;&nbsp;&nbsp; {$items[1][0]}</th>
+		<th width="5%">{$items[1][1]} </th>
+		<th width="18%">{$items[1][2]}</th>
 	</tr>
+
 	<tr>
-		<th width="62%"><table><tr><td width="40%"></td><td>BLO10000013 - Blow Shoes</td></tr></table></th>
-		<th width="15%">&nbsp;&nbsp;&nbsp; SKU</th>
-		<th width="5%">1</th>
-		<th width="18%">1</th>
+		<th width="62%"><table><tr><td width="40%"></td><td>$data->ordernr - $client</td></tr></table></th>		
+		<th width="15%">&nbsp;&nbsp;&nbsp;{$items[2][0]}</th>
+		<th width="5%">{$items[2][1]}</th>
+		<th width="18%">{$items[2][2]}</th>
 	</tr>
 	<tr>
 		<th width="62%"></th>
-		<th width="15%">&nbsp;&nbsp;&nbsp; SKU</th>
-		<th width="5%">1</th>
-		<th width="18%">1</th>
-	</tr>
-	<tr>
-		<th width="42%"></th>
-		<th width="25%"></th>
-		<th width="20%"></th>
-		<th width="35%"></th>
+		<th width="15%">&nbsp;&nbsp;&nbsp; {$items[3][0]}</th>
+		<th width="5%">{$items[3][1]}</th>
+		<th width="18%">{$items[3][2]}</th>
 	</tr>
 	<tr>
 		<th></th>
@@ -134,20 +152,20 @@ Karet Selatan
 		<tr>
 		<th width="20%">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; X</th>
 		<th width="22%"></th>
-		<th width="20%">16 01 2015</th>
+		<th width="20%">$date</th>
 		<th width="38%"></th>
 	</tr>
 	<tr>
 		<th width="50%"></th>
 		<th width="15%"></th>
 		<th width="15%"></th>
-		<th width="20%"></th>
+		<th width="20%">$number</th>
 	</tr>
 	<tr>
-		<th width="50%"></th>
+	<th width="50%"></th>
 		<th width="15%"></th>
 		<th width="15%"></th>
-		<th width="20%">200.000</th>
+		<th width="20%"></th>
 	</tr>
 	<tr>
 		<th width="50%"></th>
@@ -157,10 +175,14 @@ Karet Selatan
 	</tr>
 </table>
 EOF;
+
 // output the HTML content
 $pdf->writeHTML($html, true, false, true, false, '');
+
+endforeach;
 
 $pdf->lastPage();
 /*** end loop ***/
 
 $pdf->Output('example_028.pdf', 'I');
+?>
