5b83b042f15c3fbc6edb7eb8872c525bdddf5870
muzani <zanimumu@gmail.com>
2015-01-27 09:58:07 +0700

    revisi Client Options (TECHOPS 24)

--- application/views/form/customClient.php
+++ application/views/form/customClient.php
@@ -1,5 +1,5 @@
-<div class="panel panel-default" style="width:50%">
-  <table class="table">
+<div class="panel panel-default" style="width:100%">
+  <table class="table" >
 	<tr>
     <td>Name</td>
     <td>Value</td>   
@@ -12,8 +12,8 @@ $im = implode(" ", $ex);
 $bar = ucwords(strtolower($im));
 ?>
 	<tr>  
-    <td><?php echo $bar ?></td>
-	<td><input type="text" value="<?php echo $value[$i]['option_value']?>" name="clientoptions[option_value][<?php echo $value[$i]['id']?>]" ></td>
+    <td style="width:30%"><?php echo $bar ?></td>
+	<td><input class="form-control" type="text" value="<?php echo $value[$i]['option_value']?>" name="clientoptions[option_value][<?php echo $value[$i]['id']?>]" style="width:80%"></td>
 	<td><input type="checkbox" name="clientoptions[cek][<?php echo $i?>]" value="<?php echo $value[$i]['id']?>" ></td>
 	</tr>
 <?php }?>
