b68381900ac498ae5db137d844110e369d2a15e0
muzani <zanimumu@gmail.com>
2015-01-27 11:40:25 +0700

    revisi migrasi 11 client options (techops 24)

--- application/migrations/011_clientoptions.php
+++ application/migrations/011_clientoptions.php
@@ -7,10 +7,12 @@ class Migration_Clientoptions extends Base_migration {
 		$new= array(
 			"clientoptions" => array("name" => "Client Options", "slug" => "clientoptions", "icon" => "fa-user", "hidden" => 0, "status" => 1, "parent" => 4),
 			);
+		$newIds = array();
+		
 		$parentTags = array();
 		foreach($new as $tag => $module) {
 			$this->db->insert("module", $module);
-			$parentTags[$tag] = $this->db->insert_id();
+			$newIds[] = $parentTags[$tag] = $this->db->insert_id();
 		}
 		
 		$newModule = array(
@@ -23,7 +25,16 @@ class Migration_Clientoptions extends Base_migration {
 		
 		foreach($newModule as $module) {
 			$this->db->insert("module", $module);
+			$newIds[] = $this->db->insert_id();
 		}		
+		
+		$authData = $this->db->get_where("auth_group", array("id" => 1))->row_array();
+		$authData['auth_module'] = json_decode($authData['auth_module']);
+		$authData['auth_module'] = array_merge($authData['auth_module'], $newIds);
+		
+		$this->db->where("id", 1);
+		$this->db->update("auth_group", array("auth_module" => json_encode($authData['auth_module'])));
+		
 		$this->db->trans_complete();
 	}
 	
