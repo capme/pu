4dddcc6e09f118dced30fbaa984833a8537e9e34
Ferry Ardhana <ferry.ardhana@gmail.com>
2015-03-02 11:13:53 +0700

    New library: va_excel

---
+++ application/libraries/va_excel.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once dirname(__FILE__) ."/PHPExcel/PHPExcel.php";
+
+class Va_excel extends PHPExcel {
+    public function __construct() {
+        parent::__construct();
+    }
+}
