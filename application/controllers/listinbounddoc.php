065096020e61a953964b7aea579e7bfe738247c7
mbotok@bitbucket.org <herwindyo.purbo@velaasia.com>
2015-03-05 16:56:26 +0700

    handle error if no item selected

--- application/controllers/listinbounddoc.php
+++ application/controllers/listinbounddoc.php
@@ -235,6 +235,9 @@ class Listinbounddoc extends MY_Controller {
 	}
 	
 	public function revise(){
+		if($_GET['ids'] == ""){
+			redirect("listinbounddoc");	
+		}
 		$this->data['content'] = "form_v.php";
 		$this->data['pageTitle'] = "Inbound Document";
 		$this->data['formTitle'] = "Inbound Document - Revise";
