e4092200ef1bd9cb3e332af7afb2088fc5a39d92
mbotok@bitbucket.org <herwindyo.purbo@velaasia.com>
2015-03-09 10:25:52 +0700

    update cron for VELAMAG-64

--- application/controllers/cron/extractcatalogproduct.php
+++ application/controllers/cron/extractcatalogproduct.php
@@ -19,50 +19,56 @@ class Extractcatalogproduct extends CI_Controller {
 		
 		foreach($clients as $client) {
 			$inbound = $this->inbounddocument_m->getInboundDocumentInfo($client["id"]);
-			if(!empty($inbound)){
-				$id = $inbound['id'];
-				$doc_number = $inbound['doc_number'];
-				$client_id = $inbound['client_id'];
-				$note = $inbound['note'];
-				$type = $inbound['type'];
-				$status = $inbound['status'];
-				$created_at = $inbound['created_at'];
-				$updated_at = $inbound['updated_at'];
-				$created_by = $inbound['created_by'];
-				$filename = $inbound['filename'];
-				
-				if($status == "0"){
-					$objPHPExcel = PHPExcel_IOFactory::load($path_file."/".$filename);
-					
-					$cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
-					
-					foreach ($cell_collection as $cell) {
-					    $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
-					    $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
-					    $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
-					
-				        $arr_data[$row][$column] = $data_value;
+			if($inbound->num_rows>0){
+				foreach($inbound->result_array() as $rows ){
+					if(!empty($rows)){
+						$id = $rows['id'];
+						$doc_number = $rows['doc_number'];
+						$client_id = $rows['client_id'];
+						$note = $rows['note'];
+						$type = $rows['type'];
+						$status = $rows['status'];
+						$created_at = $rows['created_at'];
+						$updated_at = $rows['updated_at'];
+						$created_by = $rows['created_by'];
+						$filename = $rows['filename'];
+						
+						if($status == "0"){
+							$objPHPExcel = PHPExcel_IOFactory::load($path_file."/".$filename);
+							
+							$cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
+							
+							foreach ($cell_collection as $cell) {
+							    $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
+							    $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
+							    $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
+							
+						        $arr_data[$row][$column] = $data_value;
+							}
+							
+							try {
+								$return = $this->inbounddocument_m->saveToInboundInventory($client_id, $doc_number, $created_by, $arr_data);
+								echo "import inbound document for client ".$client_id." doc number ".$doc_number."<br>";
+								$return = $this->inbounddocument_m->updateStatusInboundDocumentList($id,1);
+		                        
+		                        $from=$this->session->userdata('pkUserId');	
+		                        $to=1;
+		                        $url="listinbounddoc";
+		                        $message="Inbound document was imported";
+		                        $this->notification_m->add($from, $to, $url, $message);
+		                        
+							} catch( Exception $e ) {
+								echo $e->getMessage();	
+							}
+							
+						}					
 					}
-					
-					try {
-						$return = $this->inbounddocument_m->saveToInboundInventory($client_id, $doc_number, $created_by, $arr_data);
-						echo "import inbound document for client ".$client_id."<br>";
-						$return = $this->inbounddocument_m->updateStatusInboundDocumentList($id);
-                        
-                        $from=$this->session->userdata('pkUserId');	
-                        $to=1;
-                        $url="listinbounddoc";
-                        $message="Inbound document was imported";
-                        $this->notification_m->add($from, $to, $url, $message);
-                        
-					} catch( Exception $e ) {
-						echo $e->getMessage();	
-					}
-					
-				}					
+				}
 			}else{
 				echo "no available inbound document need to imported for client ".$client["id"]."<br>";
 			}
+			/*
+			*/
 		}
 	}
 }
