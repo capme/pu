72d0d96c30c93b0a0f7ade440d21821112b1acfd
Ferry Ardhana <ferry.ardhana@gmail.com>
2015-04-07 16:26:39 +0700

    */ change table notification from varchar -> text */ Use static email subject instead use field message

--- application/migrations/schema/013_notification_up.sql
+++ application/migrations/schema/013_notification_up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `notification` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sender_id` int(11) NOT NULL,
   `url` varchar(50) NOT NULL,
-  `message` varchar(300) NOT NULL,
+  `message` TEXT NOT NULL,
   `read` tinyint(1) NOT NULL,
   `email` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
