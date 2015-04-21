859eddb5206894d199c396bd07e13e1530aac42c
unknown <mbotok@bitbucket.org>
2015-04-21 11:46:45 +0700

    fix_extractInboundForm

--- application/models/inbounddocument_m.php
+++ application/models/inbounddocument_m.php
@@ -1052,7 +1052,7 @@ class Inbounddocument_m extends MY_Model {
 				if(isset($arr_data[$x]['E'])){
 					$qtyInbound = $arr_data[$x]['E'];
 				}else{
-					$qtyInbound = "";
+					$qtyInbound = 0;
 				}	
 	
 				//note
