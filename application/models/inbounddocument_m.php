c1b72833dafb240ae1ca53068f3c900aa5889060
Ferry Ardhana <ferry.ardhana@gmail.com>
2015-05-18 09:28:22 +0700

    Escape string when upload inbound form

--- application/models/inbounddocument_m.php
+++ application/models/inbounddocument_m.php
@@ -1219,7 +1219,7 @@ class Inbounddocument_m extends MY_Model {
 					$strRec = "".($query->num_rows());
 				}		
 				//------------------ready for processing the query----------------------------
-				$query = $this->db->query("SELECT * FROM inb_inventory_item_".$client." WHERE sku_description='".$skuDescription."' AND doc_number=".$reference_id);
+				$query = $this->db->query("SELECT * FROM inb_inventory_item_".$client." WHERE sku_description=".$this->db->escape($skuDescription)." AND doc_number=".$reference_id);
 				$row = $query->result_array();
 				if(isset($row[0]['id'])){
 					// item_id
