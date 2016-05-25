0c0ba978805dbdd5493c362adabacda299b0d57a
capme <purbo.kusumo@acommerce.asia>
2016-05-25 22:30:44 +0700

    title

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -15,7 +15,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!-- BEGIN HEAD -->
 <head>
 <meta charset="utf-8"/>
-<title>BAYMAX | Admin Dashboard</title>
+<title>Leger Management System</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 <meta content="" name="description"/>
--- application/views/partial/head_v.php
+++ application/views/partial/head_v.php
@@ -14,7 +14,7 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!-- BEGIN HEAD -->
 <head>
 <meta charset="utf-8"/>
-<title><?php echo (@$title) ?  $title: 'Vela - Distribution and Retail Tracking'; ?></title> 
+<title>Leger Management System</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1" name="viewport"/>
 <meta content="" name="description"/>
